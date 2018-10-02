62a795dcf3309a82bff633314ef5bd87dd90a74b
heroku_mzjw7d6d <swapnilgajbhiye12@gmail.com>
2018-10-02 12:14:42 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  show=true;
+
+  submitForm(frm)
+  {
+console.log(frm);
+  }
+}
